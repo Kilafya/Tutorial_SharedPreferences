228aaa0c4f3520b58cbd3c41f01e4a23bad88b0c
drayl <sunrise.161@mail.ru>
2022-05-06 18:21:07 +0300

    finish

--- app/src/main/java/com/example/tutorial_sharedpreferences/MainActivity.kt
+++ app/src/main/java/com/example/tutorial_sharedpreferences/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.tutorial_sharedpreferences
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tutorial_sharedpreferences.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var preferences: SharedPreferences
+
+    private val preferencesListener = SharedPreferences.OnSharedPreferenceChangeListener { _, key ->
+        if (key == TEXT_VALUE)
+            binding.tvCurrentValue.text = preferences.getString(key, "")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        preferences = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE)
+        val value = preferences.getString(TEXT_VALUE, "")
+        with(binding) {
+            edValue.setText(value)
+            tvCurrentValue.text = value
+            btnSave.setOnClickListener{
+                val newValue = edValue.text.toString()
+                preferences.edit()
+                    .putString(TEXT_VALUE, newValue)
+                    .apply()
+            }
+        }
+        preferences.registerOnSharedPreferenceChangeListener(preferencesListener)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        preferences.unregisterOnSharedPreferenceChangeListener(preferencesListener)
     }
 }
---
+++ app/src/main/java/com/example/tutorial_sharedpreferences/constants.kt
@@ -0,0 +1,4 @@
+package com.example.tutorial_sharedpreferences
+
+const val APP_PREFERENCES = "APP_PREFERENCES"
+const val TEXT_VALUE = "VALUE"
